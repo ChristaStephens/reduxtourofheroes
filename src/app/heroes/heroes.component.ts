7e97482590ffcb430172bd58f50d6cd65033222b
Christa Stephens <christastephens@quickenloans.com>
2020-02-19 14:02:14 -0500

    details section

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero} from '../hero';
+import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 
 @Component({
@@ -9,15 +9,20 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
-  //import heroes from mock hereos
-heroes = HEROES ;
+  //import heroes from mock heroes
+  heroes = HEROES;
+  //assign the clicked hero from the html
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+  this.selectedHero = hero;
+}
 
-//  hero: Hero = {
-//    id: 1,
-//    name: 'Windstorm',
-//    power: 'weather',
-//    city: 'Chicago'
-//  };
+  //  hero: Hero = {
+  //    id: 1,
+  //    name: 'Windstorm',
+  //    power: 'weather',
+  //    city: 'Chicago'
+  //  };
 
   constructor() { }
 
