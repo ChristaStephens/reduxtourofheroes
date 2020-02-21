5e2d9a73d85aba9ad78157579413c3188a35832b
Christa Stephens <christastephens@quickenloans.com>
2020-02-21 13:44:11 -0500

    dashboard routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
+
+
+/* Add a default route
+When the app starts, the browser's address bar points to the web site's root.
+That doesn't match any existing route so the router doesn't navigate anywhere.
+The space below the <router-outlet> is blank.To make the app navigate to the
+dashboard automatically, add the following route to the AppRoutingModule.
+Routes array. */
 
 const routes: Routes = [
-  { path: 'heroes', component: HeroesComponent }
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'dashboard', component: DashboardComponent}
 ];
 
 @NgModule({
