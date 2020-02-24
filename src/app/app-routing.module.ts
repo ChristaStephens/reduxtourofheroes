ed252bea59f3c02c1e60b76b9722dedbd49fbad1
Christa Stephens <christastephens@quickenloans.com>
2020-02-24 09:09:50 -0500

    adding more components to hero

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 
 
@@ -10,12 +11,15 @@ When the app starts, the browser's address bar points to the web site's root.
 That doesn't match any existing route so the router doesn't navigate anywhere.
 The space below the <router-outlet> is blank.To make the app navigate to the
 dashboard automatically, add the following route to the AppRoutingModule.
-Routes array. */
+Routes array. The empty '' is always the home page. */
+
+//parameterized route. : colon in the path is a placeholder for a specific hero
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
   { path: 'heroes', component: HeroesComponent },
-  { path: 'dashboard', component: DashboardComponent}
+  { path: 'dashboard', component: DashboardComponent},
+  { path: 'detail/:id', component: HeroDetailComponent}
 ];
 
 @NgModule({
